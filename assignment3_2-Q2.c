c491bf23badfb8e3d21ca148fc1ec9b73392bb90
Avinash <kumaravi2419@gmail.com>
2025-10-14 00:35:34 +0530

    Add files via upload

---
+++ assignment3_2-Q2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main() {
+    int roll;
+    char name[50];
+    float m1,m2,m3,total,per;
+    scanf("%d",&roll);
+    scanf("%s",name);
+    scanf("%f%f%f",&m1,&m2,&m3);
+    total=m1+m2+m3;
+    per=(total/300)*100;
+    printf("Roll No: %d\n",roll);
+    printf("Name: %s\n",name);
+    printf("Total Marks: %.2f\n",total);
+    printf("Percentage: %.2f%%\n",per);
+    if(per>=60)
+        printf("Division: First");
+    else{
+        if(per>=50)
+            printf("Division: Second");
+        else{
+            if(per>=40)
+                printf("Division: Third");
+            else
+                printf("Division: Fail");
+        }
+    }
+    return 0;
+}
