1d2740c79b81b0be54513243e7dd2756bc56d793
Avinash <kumaravi2419@gmail.com>
2025-10-15 16:18:23 +0530

    Add files via upload

---
+++ assignmenrt3_3-Q1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    char c;
+    printf("Enter a single character\n");
+    scanf("%c",&c);
+    if(c>='a' && c<='z' || c>='A' && c<='Z')
+    {
+        printf("The character is an alphabet.");
+    }
+    else if(c>='0' && c<='9')
+    {
+        printf("The character is a digit.");
+    }
+    else{
+        printf("The character is a special character.");
+    }
+    return 0;
+}
