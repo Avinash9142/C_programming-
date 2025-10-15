67dd04ca98a0be0615273203257cfc45fc1e4089
Avinash <kumaravi2419@gmail.com>
2025-10-15 15:02:25 +0530

    Update assignment3_2-Q5.c

--- assignment3_2-Q5.c
+++ assignment3_2-Q5.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main() {
     int a,b,c,sum;
+    printf("Enter sides of triangle\n");
     scanf("%d%d%d",&a,&b,&c);
     sum=a+b+c;
     if(sum==180)
@@ -9,3 +10,5 @@ int main() {
         printf("The triangle is not valid.");
     return 0;
 }
+
+
